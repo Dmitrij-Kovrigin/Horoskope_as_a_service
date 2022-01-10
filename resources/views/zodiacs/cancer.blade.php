724cf8f4d4ad666641571a5cc0330b6b8d741043
dmitrij-kovrigin <dmitrij.kovrigin@gmail.com>
2022-01-10 06:21:13 +0200

    Some css changes

--- resources/views/zodiacs/cancer.blade.php
+++ resources/views/zodiacs/cancer.blade.php
@@ -14,5 +14,5 @@
                 @endforeach
             </ul>
             @endforeach
-            @endsection
 </div>
+@endsection
--- resources/views/zodiacs/pisces.blade.php
+++ resources/views/zodiacs/pisces.blade.php
@@ -2,15 +2,17 @@
 @section('content')
 
 <a href="{{route('zodiac_main')}}"> Back </a>
-<h1> {{$zodName}} </h1>
-<h3>Happiest month for Pisces is: <span style="color:green;">{{$bestMonth}}</span></h3>
-@foreach($months as $value)
-<h4>{{array_shift($monthNames)}}
-    <h4>
-        <ul class="days">
-            @foreach($value as $val)
-            <li style="background: {{$colors[$val->pisces_score]}}"><a href="{{route('zodiac_text', [$val->date, $zodName, $val->pisces_score])}}" target="_blank">{{$val->date}} Score: {{$val->pisces_score}}</a></li>
+<div class="margin3_5">
+    <h1> {{$zodName}} </h1>
+    <h3>Happiest month for Pisces is: <span style="color:green;">{{$bestMonth}}</span></h3>
+    @foreach($months as $value)
+    <h4>{{array_shift($monthNames)}}
+        <h4>
+            <ul class="days">
+                @foreach($value as $val)
+                <li style="background: {{$colors[$val->pisces_score]}}"><a href="{{route('zodiac_text', [$val->date, $zodName, $val->pisces_score])}}" target="_blank">{{$val->date}} Score: {{$val->pisces_score}}</a></li>
+                @endforeach
+            </ul>
             @endforeach
-        </ul>
-        @endforeach
-        @endsection
+</div>
+@endsection
